1f53d23a87731cfc3911336812a37a869fad8118
Ashraf <moolla.ash@gmail.com>
2013-05-10 17:56:50 +0200

    Changed fixed length fields from VARCHAR to CHAR

--- setupDb.py
+++ setupDb.py
@@ -1,7 +1,7 @@
 from dbproj.db import connect_database
 
 sql = '''CREATE TABLE IF NOT EXISTS Users (
-	Regnum VARCHAR(9) PRIMARY KEY,
+	Regnum CHAR(9) PRIMARY KEY,
 	FullName VARCHAR(50) NOT NULL,
 	Salt VARCHAR (20) NOT NULL,
 	HashedPassword VARCHAR(40),
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS Questions (
 	Qno INTEGER PRIMARY KEY,
 	Question VARCHAR(100) NOT NULL,
 	RightAnswer VARCHAR(20) NOT NULL,
-	SetBy VARCHAR(9) NOT NULL,
+	SetBy CHAR(9) NOT NULL,
 	Useless BOOLEAN,
 	Difficulty INTEGER CONSTRAINT ValidDifficulty CHECK (Difficulty >=0 AND Difficulty <= 3))
 );
 
 CREATE TABLE IF NOT EXISTS MCQans (
 	Qno INTEGER REFERENCES Questions (Qno),
-	Letter VARCHAR(1) NOT NULL,
+	Letter CHAR(1) NOT NULL,
 	Answer VARCHAR(20) NOT NULL,
 
 	PRIMARY KEY( Qno, Letter )
@@ -35,8 +35,8 @@ CREATE TABLE IF NOT EXISTS Answers (
 
 CREATE TABLE IF NOT EXISTS Ratings (
 	Qno INTEGER REFERENCES Questions (Qno),
-	Regnum VARCHAR(9) REFERENCES Users (Regnum),
-	Points INTEGER CONSTRAINT ValidRatingScore CHECK (Points < 100 AND Points > 0),
+	Regnum CHAR(9) REFERENCES Users (Regnum),
+	Points INTEGER CONSTRAINT ValidRatingScore CHECK (Points <= 100 AND Points > 0),
 	Reason VARCHAR(50),
 
 	PRIMARY KEY( Qno, Regnum )
